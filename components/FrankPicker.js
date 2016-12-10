451bfd3fe661b81f8b4f416c1200dd613b1f85ec
Gavin Morgan <quavmo@gmail.com>
2016-12-10 13:29:56 -0800

    frankpicker displays

--- components/FrankPicker.js
+++ components/FrankPicker.js
@@ -1,14 +1,28 @@
-import { pick } from 'ramda';
+import { random, image } from 'faker';
+import { pick, times, map } from 'ramda';
 import { connect } from 'react-redux';
 import { createElement as el } from 'react';
-import { Drawer } from 'material-ui'
+import { Drawer, MenuItem, Avatar } from 'material-ui'
 import * as mapDispatchToProps from '../core/actions';
+
+const frankData = times(() => ({
+  image: image.business(),
+  title: random.word()
+}), 5);
+
+const hydrateMenuItem = ({image, title}) => 
+  el(MenuItem, {
+    key: title,
+    primaryText: title,
+    leftIcon: el(Avatar, {src: image}),
+    onTouchTap: () => focusFrank(title)
+  })
 
 const FrankPicker = ({drawer, toggleDrawer}) => {
   return el(Drawer, {
     open: drawer.open, docked: false,
     onRequestChange:  () => toggleDrawer()
-  });
+  }, map(hydrateMenuItem, frankData));
 }
 
 export default connect(pick(['drawer']), mapDispatchToProps)(FrankPicker);
